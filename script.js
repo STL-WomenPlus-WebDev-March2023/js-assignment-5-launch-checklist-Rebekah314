1e1419ceb9f5f3fee07ea357f52a708e391964e0
Rebekah Garris <rebekahlgarris@gmail.com>
2023-06-29 14:11:17 -0500

    moved input validation and status update codes into formSubmission function

--- script.js
+++ script.js
@@ -14,52 +14,19 @@ window.addEventListener("load", function() {
     const copilot = document.querySelector("input[name=copilotName]");
     const fuelLevel = document.querySelector("input[name=fuelLevel]");
     const cargoMass = document.querySelector("input[name=cargoMass]");
-    let list = "";
 
-    let faultyItemsList = document.getElementById("faultyItems");
+    let list = document.getElementById("faultyItems");
     let pilotStatus = document.getElementById("pilotStatus");
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
     let cargoStatus = document.getElementById("cargoStatus");
     let launchStatus = document.getElementById("launchStatus");
 
-    faultyItemsList.style.visibility = "hidden";
+    list.style.visibility = "hidden";
 
     submitButton.addEventListener("click", function(event) {
         
-        let checkFields = formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
-        
-        //Make sure no fields are left blank or are incorrect type
-        if (checkFields === "empty") {
-            alert("All fields required!");
-        } else if (checkFields === "wrong type") {
-            alert("Make sure to enter valid information for each field!");
-        } else {
-            faultyItemsList.style.visibility = "visible";
-            pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch`;
-            copilotStatus.innerHTML = `Co-pilot ${copilot.value} is ready for launch`;
-            //Update faultyItems list
-            if (checkFields.includes("fuel") || checkFields.includes("cargo")) {
-                launchStatus.innerHTML = "Shuttle Not Ready For Launch";
-                launchStatus.style.color = "rgb(199, 37, 78)";
-                if (checkFields.includes("fuel")) {
-                    fuelStatus.innerHTML = "Fuel level too low for launch";
-                } else {
-                    fuelStatus.innerHTML = "Fuel level high enough for launch";
-                }
-                if (checkFields.includes("cargo")) {
-                    cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-                } else {
-                    cargoStatus.innerHTML = "Cargo mass low enough for launch";
-                }
-            } else {
-                launchStatus.innerHTML = "Shuttle is Ready for Launch";
-                launchStatus.style.color = "rgb(65, 159, 106)";
-                cargoStatus.innerHTML = "Cargo mass low enough for launch";
-                fuelStatus.innerHTML = "Fuel level high enough for launch";
-                
-            }
-        }
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
         
         event.preventDefault();
     });
--- scriptHelper.js
+++ scriptHelper.js
@@ -36,22 +36,39 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    
    if (checkPilot === "Empty" || checkCopilot === "Empty" 
    || checkFuel === "Empty" || checkCargo === "Empty") {
-    return "empty";
-   }
-
-   if (checkPilot === "Is a Number" || checkCopilot === "Is a Number" 
+        alert("All fields required!");
+   } else if (checkPilot === "Is a Number" || checkCopilot === "Is a Number" 
    || checkFuel === "Not a Number" || checkCargo === "Not a NUmber") {
-    return "wrong type";
-   }
+        alert("Make sure to enter valid information for each field!");
+   } else {
+        list.style.visibility = "visible";
+        pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch`;
+        copilotStatus.innerHTML = `Co-pilot ${copilot.value} is ready for launch`;
 
-   let returnString = "";
-   if (fuelLevel.value < 10000){
-    returnString += "fuel ";
-   }
-   if (cargoLevel.value > 10000) {
-    returnString += "cargo";
-   }
-   return returnString;
+
+
+        if (fuelLevel.value < 10000 || cargoLevel.value > 10000) {
+            launchStatus.innerHTML = "Shuttle Not Ready For Launch";
+            launchStatus.style.color = "rgb(199, 37, 78)";
+            if (fuelLevel.value < 10000) {
+                fuelStatus.innerHTML = "Fuel level too low for launch";
+            } else {
+                fuelStatus.innerHTML = "Fuel level high enough for launch";
+            }
+            if (cargoLevel.value > 10000) {
+                cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+            } else {
+                cargoStatus.innerHTML = "Cargo mass low enough for launch";
+            }
+        } else {
+            launchStatus.innerHTML = "Shuttle is Ready for Launch";
+            launchStatus.style.color = "rgb(65, 159, 106)";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+            
+        }
+
+   } 
 }
 
 async function myFetch() {
